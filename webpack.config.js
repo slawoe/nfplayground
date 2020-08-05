84485a4306ea32fa3fd41b98eb5e7d334e636775
slawoe <info@slawomirernst.de>
2020-08-05 15:48:55 +0200

    Add css and image import

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,21 @@ const path = require("path");
 module.exports = {
   entry: "./src/app.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
   },
   mode: "production",
 };
